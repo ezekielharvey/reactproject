f07b80a4012ca94e3fa97576e753f28ae7f34a3b
ezekielharvey <zekeharvey@outlook.com>
2022-05-20 18:44:41 -0400

    Fixed PropType Bug

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -14,7 +14,7 @@ const Pagination = props => {
             <ul className="pagination justify-content-center">
                 {pages.map(page => (
                     <li key={page} className={page === currentPage ? 'page-item active' : 'page-item'}>
-                        <a className="page-link" onClick={() => props.onPageChange(page)}>{page}</a>
+                        <a className="page-link" onClick={() => onPageChange(page)}>{page}</a>
                     </li>
                 ))}
             </ul>
@@ -25,8 +25,8 @@ const Pagination = props => {
 Pagination.propTypes = {
     itemsCount: PropTypes.number.isRequired,
     pageSize: PropTypes.number.isRequired,
-    onPageChange: PropTypes.number.isRequired,
-    currentPage: PropTypes.func.isRequired,
+    onPageChange: PropTypes.func.isRequired,
+    currentPage: PropTypes.number.isRequired,
 }
 
 export default Pagination;
