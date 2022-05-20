810e4cff40a9db2bff7cee217af29254de664463
ezekielharvey <zekeharvey@outlook.com>
2022-05-20 17:18:09 -0400

    Added Genre Selection

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
 const ListGroup = (props) => {
-    const { items } = props;
+    const { items, textProperty, valueProperty, selectedItem, onItemSelect } = props;
 
     return <ul className="list-group">
-        {items.map(item => <li key={item._id} className="list-group-item">{item.name}</li>)}
-
+        {items.map(item => (
+            <li
+                onClick={() => onItemSelect(item)}
+                key={item[valueProperty]}
+                className="list-group-item">{item[textProperty]}</li>
+        ))};
     </ul>;
-}
+};
+
+ListGroup.defaultProps = {
+    textProperty: 'name',
+    valueProperty: '_id',
+};
 
 export default ListGroup;
