8bc9a8dfa97dcba236301fbb2cbac3b276e5ae35
ezekielharvey <zekeharvey@outlook.com>
2022-05-20 20:54:29 -0400

    Bug Fix

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -48,7 +48,7 @@ class Movies extends Component {
 
         return (
             <div className='row'>
-                <div className='col-2'>
+                <div className='col-3'>
                     <ListGroup
                         items={this.state.genres}
                         selectedItem={this.state.selectedGenre}
