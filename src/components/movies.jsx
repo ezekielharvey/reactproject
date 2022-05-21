d063e1221ec1abb18709266d8ee2703d33ce43c8
ezekielharvey <zekeharvey@outlook.com>
2022-05-21 12:37:22 -0400

    Added All Genres Filter

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -14,7 +14,9 @@ class Movies extends Component {
     };
 
     componentDidMount() {
-        this.setState({ movies: getMovies(), genres: getGenres() });
+        const genres = [{ name: 'All Genres' }, ...getGenres()];
+
+        this.setState({ movies: getMovies(), genres });
     };
 
     handleDelete = movie => {
@@ -40,11 +42,15 @@ class Movies extends Component {
 
     render() {
         const { length: count } = this.state.movies;
-        const { pageSize, currentPage, movies: allMovies } = this.state;
+        const { pageSize, currentPage, selectedGenre, movies: allMovies } = this.state;
 
         if (count === 0) return <p>There are no movies in the database.</p>;
 
-        const movies = paginate(allMovies, currentPage, pageSize)
+        const filtered = selectedGenre && selectedGenre._id
+            ? allMovies.filter(m => m.genre._id === selectedGenre._id)
+            : allMovies;
+
+        const movies = paginate(filtered, currentPage, pageSize)
 
         return (
             <div className='row'>
@@ -56,7 +62,7 @@ class Movies extends Component {
                     />
                 </div>
                 <div className='col'>
-                    <p>Showing {this.state.movies.length} movies in the database.</p>
+                    <p>Showing {filtered.length} movies in the database.</p>
                     <table className="table">
                         <thead>
                             <tr>
@@ -84,7 +90,7 @@ class Movies extends Component {
                         </tbody>
                     </table>
                     <Pagination
-                        itemsCount={count}
+                        itemsCount={filtered.length}
                         pageSize={pageSize}
                         currentPage={currentPage}
                         onPageChange={this.handlePageChange} />
