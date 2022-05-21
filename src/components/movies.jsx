06a5c7400f60a30634969f14679dd55be63eed97
ezekielharvey <zekeharvey@outlook.com>
2022-05-21 16:02:47 -0400

    Moved sort function to MoviesTable.jsx

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
 import { getGenres } from '../services/fakeGenreService';
-import Like from "./common/like";
 import ListGroup from './common/listGroup';
 import Pagination from "./common/pagination";
 import { paginate } from '../utils/paginate'
+import MoviesTable from "./moviesTable";
+import _ from "lodash";
 class Movies extends Component {
     state = {
         movies: [],
         genres: [],
         currentPage: 1,
         pageSize: 5,
+        sortColumn: { path: 'title', order: 'asc' }
     };
 
     componentDidMount() {
-        const genres = [{ name: 'All Genres' }, ...getGenres()];
+        const genres = [{ _id: '', name: 'All Genres' }, ...getGenres()];
 
         this.setState({ movies: getMovies(), genres });
     };
@@ -37,12 +39,16 @@ class Movies extends Component {
     };
 
     handleGenreSelect = genre => {
-        this.setState({ selectedGenre: genre });
+        this.setState({ selectedGenre: genre, currentPage: 1 });
     }
+
+    handleSort = sortColumn => {
+        this.setState({ sortColumn });
+    };
 
     render() {
         const { length: count } = this.state.movies;
-        const { pageSize, currentPage, selectedGenre, movies: allMovies } = this.state;
+        const { pageSize, currentPage, selectedGenre, sortColumn, movies: allMovies } = this.state;
 
         if (count === 0) return <p>There are no movies in the database.</p>;
 
@@ -50,7 +56,9 @@ class Movies extends Component {
             ? allMovies.filter(m => m.genre._id === selectedGenre._id)
             : allMovies;
 
-        const movies = paginate(filtered, currentPage, pageSize)
+        const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
+
+        const movies = paginate(sorted, currentPage, pageSize)
 
         return (
             <div className='row'>
@@ -63,32 +71,12 @@ class Movies extends Component {
                 </div>
                 <div className='col'>
                     <p>Showing {filtered.length} movies in the database.</p>
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th>Title</th>
-                                <th>Genre</th>
-                                <th>Stock</th>
-                                <th>Rate</th>
-                                <th></th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {movies.map(movie => (
-                                <tr key={movie._id}>
-                                    <td>{movie.title}</td>
-                                    <td>{movie.genre.name}</td>
-                                    <td>{movie.numberInStock}</td>
-                                    <td>{movie.dailyRentalRate}</td>
-                                    <td>
-                                        <Like liked={movie.liked} onClick={() => this.handleLike(movie)} />
-                                    </td>
-                                    <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                    <MoviesTable
+                        movies={movies}
+                        sortColumn={sortColumn}
+                        onLike={this.handleLike}
+                        onDelete={this.handleDelete}
+                        onSort={this.handleSort} />
                     <Pagination
                         itemsCount={filtered.length}
                         pageSize={pageSize}
