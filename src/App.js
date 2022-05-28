840ad75e2a3500a425160b453660ea19547b7b2d
ezekielharvey <zekeharvey@outlook.com>
2022-05-28 11:04:19 -0400

    Browser Import

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
-import Movies from "./components/movies";
+import Customers from './components/customers';
+import Movies from './components/movies';
+import NotFound from './components/notFound';
+import Rentals from './components/rentals';
 
 class App extends Component {
   render() {
     return (
       <main className="container">
-        <Movies />
+        <Switch>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
       </main>
     );
   }
