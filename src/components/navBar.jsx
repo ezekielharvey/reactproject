b620ff726640d17368009c740d2455826b98936a
ezekielharvey <zekeharvey@outlook.com>
2022-05-28 12:40:37 -0400

    Bug Fix

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -4,39 +4,33 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <Link class="navbar-brand" to="/movies">
-        Vidly
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+        Navbar
       </Link>
       <button
-        class="navbar-toggler"
+        className="navbar-toggler"
         type="button"
         data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span class="navbar-toggler-icon"></span>
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <NavLink class="nav-link" to="/movies">
-              Movies <span class="sr-only">(current)</span>
-            </NavLink>
-          </li>
-          <li class="nav-item">
-            <NavLink class="nav-link" to="/customers">
-              Customers
-            </NavLink>
-          </li>
-          <li class="nav-item">
-            <NavLink class="nav-link" to="/rentals">
-              Rentals
-            </NavLink>
-          </li>
-        </ul>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <NavLink className="nav-item nav-link" to="/movies">
+            Movies <span className="sr-only">(current)</span>
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/customers">
+            Customers
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/rentals">
+            Rentals
+          </NavLink>
+        </div>
       </div>
     </nav>
   );
