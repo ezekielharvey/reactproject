09cbd61c68e0a29392a4e82f38409d35a6719a09
ezekielharvey <zekeharvey@outlook.com>
2022-05-28 12:06:20 -0400

    Format Change

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -3,29 +3,33 @@ import _ from 'lodash';
 import PropTypes from 'prop-types';
 
 const Pagination = ({ itemsCount, pageSize, onPageChange, currentPage }) => {
+  const pagesCount = Math.ceil(itemsCount / pageSize);
+  if (pagesCount === 1) return null;
+  const pages = _.range(1, pagesCount + 1);
 
-    const pagesCount = Math.ceil(itemsCount / pageSize);
-    if (pagesCount === 1) return null;
-    const pages = _.range(1, pagesCount + 1)
-
-    return (
-        <nav>
-            <ul className="pagination justify-content-center">
-                {pages.map(page => (
-                    <li key={page} className={page === currentPage ? 'page-item active' : 'page-item'}>
-                        <a className="page-link" onClick={() => onPageChange(page)}>{page}</a>
-                    </li>
-                ))}
-            </ul>
-        </nav>
-    );
-}
+  return (
+    <nav>
+      <ul className="pagination justify-content-center">
+        {pages.map((page) => (
+          <li
+            key={page}
+            className={page === currentPage ? 'page-item active' : 'page-item'}
+          >
+            <a className="page-link" onClick={() => onPageChange(page)}>
+              {page}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
 Pagination.propTypes = {
-    itemsCount: PropTypes.number.isRequired,
-    pageSize: PropTypes.number.isRequired,
-    onPageChange: PropTypes.func.isRequired,
-    currentPage: PropTypes.number.isRequired,
-}
+  itemsCount: PropTypes.number.isRequired,
+  pageSize: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  currentPage: PropTypes.number.isRequired,
+};
 
 export default Pagination;
