36cbc3bcc48dffa074966ce8455a62e898f0c35b
ezekielharvey <zekeharvey@outlook.com>
2022-05-28 11:44:01 -0400

    Added NavBar Component

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import { Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
 import Customers from './components/customers';
 import Movies from './components/movies';
+import NavBar from './components/navBar';
 import NotFound from './components/notFound';
 import Rentals from './components/rentals';
 
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        <Switch>
-          <Route path="/movies" component={Movies}></Route>
-          <Route path="/customers" component={Customers}></Route>
-          <Route path="/rentals" component={Rentals}></Route>
-          <Route path="/not-found" component={NotFound}></Route>
-          <Redirect from="/" to="/movies" />
-          <Redirect to="/not-found" />
-        </Switch>
-      </main>
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/movies" component={Movies}></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect from="/" to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
