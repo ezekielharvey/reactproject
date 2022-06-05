703ca15fd670886be48d816d823779c7880dbaf6
Ezekiel Harvey <ezekielharvey@icloud.com>
2022-06-05 17:35:47 -0400

    New Component

---
+++ src/components/registerForm.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Joi from 'joi-browser';
+import Form from './common/form'
+
+class RegisterForm extends Form {
+    state = {
+        data: {email: '', password: '', name: ''},
+        errors: {},
+      }
+
+    schema = {
+        email: Joi.string()
+            .required()
+            .email()
+            .label('Email'),
+        password: Joi.string()
+            .required()
+            .min(5)
+            .label('Password'),
+        name: Joi.string()
+            .required()
+            .label('Name'),
+    }
+
+    doSubmit = () => {
+        console.log("Submitted successfully");
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>Register</h1>
+                <form onSubmit={this.handleSubmit}>
+                    {this.renderInput('email', 'Email')}
+                    {this.renderInput('password', 'Password', 'password')}
+                    {this.renderInput('name', 'Name')}
+                    {this.renderButton('Register')}
+                </form>
+            </div>
+            
+        );
+    }
+}
+
+export default RegisterForm;
